24b1b8312a0197e6e66e530deb95607da8ef55d4
taniusa90 <galea_tanea@mail.com>
2022-08-15 11:39:59 -0500

    Lambda

---
+++ m3-lambda/src/main/java/com/cydeo/BubbleSort.java
@@ -0,0 +1,7 @@
+package com.cydeo;
+
+public class BubbleSort implements Sorting{
+    public void sort() {
+        System.out.println("Bubble Sorting");
+    }
+}
---
+++ m3-lambda/src/main/java/com/cydeo/QuickSort.java
@@ -0,0 +1,7 @@
+package com.cydeo;
+
+public class QuickSort implements  Sorting{
+    public void sort() {
+        System.out.println("Quick Sorting ");
+    }
+}
