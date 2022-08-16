60098011ba5f5ddfb18a53bf00c0dd3786b39d03
taniusa90 <galea_tanea@mail.com>
2022-08-15 21:20:09 -0500

    Enum

--- m2-enum/src/main/java/com/cydeo/Currency.java
+++ m2-enum/src/main/java/com/cydeo/Currency.java
@@ -4,9 +4,13 @@ public enum Currency {  //special class that hold only constants
 
     PENNY(1),NICKLE(5),DIME(10),QUARTER(25);//Penny is a constant object of a Currency class ;
 
-    int value;
+    private int value;
 
     Currency(int value) {
         this.value = value;
+    }
+
+    public int getValue() {
+        return value;
     }
 }
