f92ce00753e257ffcdebf21a330c13ef4543fae1
taniusa90 <galea_tanea@mail.com>
2022-08-18 11:07:39 -0500

    Lambda Task

--- m3-lambda/src/main/java/com/cydeo/tasks/OrangeTest.java
+++ m3-lambda/src/main/java/com/cydeo/tasks/OrangeTest.java
@@ -5,6 +5,7 @@ import com.cydeo.Color;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 public class OrangeTest {
 
@@ -30,6 +31,18 @@ public class OrangeTest {
         prettyPrintApple(inventory,fancyFormatter);
 
 
+        Function<Orange,String>function=orange -> "An orange of "+orange.getWeight()+" g";
+        printOrangeWithFunction(inventory,function);
+        System.out.println("----------");
+        Function<Orange,String>function1=orange -> {
+            String ch = orange.getWeight()>200 ? "Heavy" : "Light";
+            return "A "+ ch+" "+orange.getColor()+" orange";
+
+        };
+
+        printOrangeWithFunction(inventory,function1);
+
+
 
 
 
@@ -43,5 +56,14 @@ public class OrangeTest {
         }
 
 
+    }
+
+    public static void printOrangeWithFunction(List<Orange> orangeList, Function<Orange,String>function){
+
+        for (Orange orange : orangeList){
+            String output=function.apply(orange);
+            System.out.println(output);
+
+        }
     }
 }
