9d8eb96d326c4138d92b0629da54be08e0e806cb
taniusa90 <galea_tanea@mail.com>
2022-08-18 22:28:51 -0500

    Comparator Interface

---
+++ M5-comparator_interface/src/main/java/com/cydeo/MyComparator.java
@@ -0,0 +1,12 @@
+package com.cydeo;
+
+import java.util.Collections;
+import java.util.Comparator;
+
+public class MyComparator implements Comparator<Integer> {
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return (01>02) ?-1 :(01<02) ? 1 :0;
+
+    }
+}
